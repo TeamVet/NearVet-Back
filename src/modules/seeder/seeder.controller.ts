79c5f0d31a8088f5e4214343fd05259765ab7065
facuzero <facuzero_95@hotmail.com>
2024-08-21 15:43:09 -0300

    seeders usuarios, roles, mascotas, veterinarios

---
+++ src/modules/seeder/seeder.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { SeederService } from './seeder.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('seeder')
+@Controller('seeder')
+export class SeederController {
+  constructor(private readonly seederService: SeederService) {}
+
+  @Post('pets')
+  @ApiOperation({ summary: 'Cargar archivo de mascotas' })
+  addPets() {
+    return this.seederService.loadPetsData();
+  }
+
+  @Post('roles')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Cargar archivo de roles' })
+  addRoles() {
+    return this.seederService.loadRolesData();
+  }
+
+  @Post('user')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Cargar archivo de usuarios' })
+  addUsers() {
+    return this.seederService.loadUsersData();
+  }
+
+  @Post('veterinaries')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiOperation({ summary: 'Cargar archivo de veterinarios' })
+  addVeterinaries() {
+    return this.seederService.loadVeterinariesData();
+  }
+
+  @Post('reset')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Borra los datos de productos y los vuelva a cargar',
+  })
+  @ApiResponse({ status: 200, description: 'Datos reiniciados correctamente' })
+  @ApiResponse({
+    status: 403,
+    description:
+      'No se pueden reiniciar los datos porque hay claves foraneas realcionadas',
+  })
+  resetData() {
+    return this.seederService.resetData();
+  }
+}
