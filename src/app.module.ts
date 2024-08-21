31912e2fbd4764c8c7c15864a9694e903bc6b27d
facuzero <facuzero_95@hotmail.com>
2024-08-21 16:42:46 -0300

    notificaciones con nodemailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,34 +2,36 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { PetsModule } from './modules/pets/pets.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import typeOrmConfig from "./config/typeorm"
+import typeOrmConfig from './config/typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthGlobalModule } from './modules/authGlobal/authGlobal.module';
-
+import { EmailService } from './services/email/email.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [typeOrmConfig]
-      }),
-   TypeOrmModule.forRootAsync({ 
-      inject:[ConfigService],
-      useFactory: (configService: ConfigService) => configService.get("typeorm"), 
-      }),
+      load: [typeOrmConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
 
-      // modulo para generar los token
-      JwtModule.register({
-        global: true,
-        signOptions: {expiresIn : "1h"},
-        secret: process.env.JWT_SECRET,
-      }),
+    // modulo para generar los token
+    JwtModule.register({
+      global: true,
+      signOptions: { expiresIn: '1h' },
+      secret: process.env.JWT_SECRET,
+    }),
 
-      AuthGlobalModule, UsersModule, PetsModule],
+    AuthGlobalModule,
+    UsersModule,
+    PetsModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [EmailService],
 })
-export class AppModule {
-
-}
+export class AppModule {}
