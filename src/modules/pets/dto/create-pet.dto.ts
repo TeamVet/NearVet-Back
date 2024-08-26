be7c70ec7eff5c3ae0cb157073f0580ed766d96a
EAEzequielAlonso <eze.g.alonso@gmail.com>
2024-08-26 00:39:53 -0300

    ParseUUID a params - suma de atributos a entidad pets - creacion de repCondition

--- src/modules/pets/dto/create-pet.dto.ts
+++ src/modules/pets/dto/create-pet.dto.ts
@@ -1,41 +1,40 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   IsString,
   IsUUID,
   Length,
   IsOptional,
+  IsDateString,
+  IsNumber,
 } from 'class-validator';
 
 export class CreatePetDto {
-  @ApiProperty({
-    description: 'El nombre es obligatorio',
-    example: 'Firulais',
-  })
+  @ApiProperty({description: 'El nombre es obligatorio', example: 'Firulais'})
   @IsNotEmpty()
   @IsString()
   @Length(1, 50)
   name: string;
 
-  @ApiProperty({
-    description: 'La fecha de nacimiento es obligatoria',
-    example: '01-07-2024',
+  @ApiPropertyOptional({
+    description: 'La fecha de nacimiento no es obligatoria',
+    example: new Date('01-07-2024'),
   })
-  @IsNotEmpty()
-  @IsString()
-  birthdate: string;
+  @IsOptional()
+  @IsDateString()
+  birthdate?: Date;
 
   @ApiProperty({
     description: 'La fecha de ingreso en el sistema es obligatoria',
-    example: '01-08-2024',
+    example: new Date('01-08-2024'),
   })
   @IsNotEmpty()
-  @IsString()
-  startDate: string;
+  @IsDateString()
+  startDate: Date;
 
-  @IsNotEmpty()
-  @IsString()
-  endDate: string;
+  @IsOptional()
+  @IsDateString()
+  endDate?: Date;
 
   @ApiProperty({
     description: 'El color es obligatorio',
@@ -46,18 +45,46 @@ export class CreatePetDto {
   @Length(1, 10)
   color: string;
 
+  @ApiPropertyOptional({description: 'El peso actual no es obligatorio', example: 8.5})
+  @IsOptional()
+  @IsNumber()
+  weightCurrent?: Number;
+
+  @ApiPropertyOptional({description: 'La Observacion no es obligatoria', example: "Suele ser un perro agresivo"})
+  @IsOptional()
+  @IsString()
+  observation?: string;
+
+  @ApiPropertyOptional({description: 'La imagen no es obligatorio, Es un enlace', example: "https://www.perritos.com/foto.jpg"})
+  @IsOptional()
+  @IsString()
+  image?: string;
+
+  @ApiProperty({
+    description: "Este es el ID del usuario dueño de la mascota",
+    example: "21131006-7eae-47f8-93c2-1264c6be49cb"
+  })
+  @IsNotEmpty()
   @IsUUID()
   userId: string;
 
+  @ApiPropertyOptional({description: 'El id de la especie no es obligatorio', example: "21131006-7eae-47f8-93c2-1264c6be49cb"})
   @IsOptional()
   @IsUUID()
   specieId?: string;
 
+  @ApiPropertyOptional({description: 'El id de la raza no es obligatorio', example: "21131006-7eae-47f8-93c2-1264c6be49cb"})
   @IsOptional()
   @IsUUID()
   raceId?: string;
 
+  @ApiPropertyOptional({description: 'El id del sexo no es obligatorio', example: "21131006-7eae-47f8-93c2-1264c6be49cb"})
   @IsOptional()
   @IsUUID()
   sexId?: string;
+
+  @ApiPropertyOptional({description: 'El id de la condicion reproductiva no es obligatorio', example: "21131006-7eae-47f8-93c2-1264c6be49cb"})
+  @IsOptional()
+  @IsUUID()
+  repConditionId?: string;
 }
--- src/modules/pets/entities/pet.entity.ts
+++ src/modules/pets/entities/pet.entity.ts
@@ -1,50 +1,42 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, EntitySchema, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Specie } from './specie.entity';
 import { Race } from './race.entity';
 import { User } from 'src/modules/users/entities/user.entity';
 import { Sex } from './sex.entity';
+import { RepCondition } from './repCondition.entity';
 
 @Entity({
   name: 'pets',
 })
 export class Pet {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn('uuid') 
   id: string;
 
-  @Column({
-    type: 'varchar',
-    length: 50,
-    nullable: false,
-  })
+  @Column({type: 'varchar', length: 50, nullable: false}) 
   name: string;
 
-  @Column({
-    type: 'varchar',
-    length: 50,
-    nullable: false,
-  })
-  birthdate: string;
+  @Column({type: 'date', nullable: true}) 
+  birthdate: Date;
 
-  @Column({
-    type: 'varchar',
-    length: 50,
-    nullable: false,
-  })
-  startDate: string;
+  @Column({ type: 'date', nullable: false}) 
+  startDate: Date;
 
-  @Column({
-    type: 'varchar',
-    length: 50,
-    nullable: false,
-  })
-  endDate: string;
+  @Column({type: 'date',nullable: true})
+  endDate: Date;
 
-  @Column({
-    type: 'varchar',
-    length: 10,
-    nullable: false,
-  })
+  @Column({type: 'varchar', length: 10, nullable: false})
   color: string;
+
+  @Column({type: 'varchar', nullable: true})
+  observation: string;
+
+  @Column({type: 'decimal', precision: 10, scale: 2, nullable: true})
+  weightCurrent: Number;
+
+  @Column({type: 'varchar', nullable: false,
+    default: `https://us.123rf.com/450wm/findriyani/findriyani2301/findriyani230100821/197733705-ilustraci%C3%B3n-de-vector-de-logotipo-de-silueta-de-tienda-de-mascotas.jpg?ver=6`,
+  })
+  image: string;
 
   /* RELACION MUCHOS-A-UNO CON usuarios */
   @ManyToOne(() => User, (user) => user.pets)
@@ -61,4 +53,8 @@ export class Pet {
   /* RELACION MUCHOS-A-UNO CON sexo */
   @ManyToOne(() => Sex, (sex) => sex.pets)
   sex: Sex;
+
+  /* RELACION MUCHOS-A-UNO CON Condicion Reproductiva */
+  @ManyToOne(() => RepCondition, (repCondition) => repCondition.pets)
+  repCondition: RepCondition;
 }
