2937d92feb2fa7c9fad46533f7bc1b2d68548fca
EAEzequielAlonso <eze.g.alonso@gmail.com>
2024-09-13 11:28:58 -0300

    precarga minima

--- src/modules/clinical-examination/clinical-examination.service.ts
+++ src/modules/clinical-examination/clinical-examination.service.ts
@@ -1,12 +1,16 @@
 import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { ClinicalExaminationRepository } from './clinical-examination.repository';
 import { ClinicalExamination } from './entities/clinicalExamination.entity';
-import { DeleteResult, UpdateResult } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { Veterinarian } from '../veterinarian/entities/veterinarian.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ClinicalExaminationService {
 
-  constructor (private readonly examinationRepository: ClinicalExaminationRepository) {}
+  constructor (private readonly examinationRepository: ClinicalExaminationRepository,
+    @InjectRepository(Veterinarian) private veterinarianRepository: Repository<Veterinarian>,
+  ) {}
 
   async getExaminations (page:number, limit:number): Promise<ClinicalExamination[]> {
     return await this.examinationRepository.getExaminations(page,limit)
@@ -27,7 +31,8 @@ export class ClinicalExaminationService {
   }
 
   async createExamination (examination:Partial<ClinicalExamination>): Promise<ClinicalExamination> {
-    const examinationCreated: ClinicalExamination = await this.examinationRepository.createExamination(examination)
+    const veterinarian: Veterinarian = await this.veterinarianRepository.findOne({where: {userId:examination.veterinarianId}});
+    const examinationCreated: ClinicalExamination = await this.examinationRepository.createExamination({...examination, veterinarianId: veterinarian.id})
     if (!examinationCreated) throw new InternalServerErrorException("No se pudo crear la Atencion Medica");
     // crear una orden de venta....
     return examinationCreated
