ea69cb1dc0aa90acee411db60e314948b616e505
EAEzequielAlonso <eze.g.alonso@gmail.com>
2024-09-18 00:13:34 -0300

    update DTO de Sales

--- src/modules/sales/dto/update-sale.dto.ts
+++ src/modules/sales/dto/update-sale.dto.ts
@@ -1,4 +1,41 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSaleDto } from './create-sale.dto';
+import { ApiPropertyOptional } from "@nestjs/swagger"
+import { IsBoolean, IsDateString, IsNumber, IsOptional, IsUUID } from "class-validator"
 
-export class UpdateSaleDto extends PartialType(CreateSaleDto) {}
+export class UpdateSaleDto {
+
+    @IsNumber()
+    @IsOptional()
+    @ApiPropertyOptional()
+    discount?: number
+
+    @IsNumber()
+    @IsOptional()
+    @ApiPropertyOptional()
+    total?: number
+
+    @IsDateString()
+    @IsOptional()
+    @ApiPropertyOptional()
+    date?: Date
+
+    @IsDateString()
+    @IsOptional()
+    @ApiPropertyOptional()
+    advancedPay?: number
+
+    @IsUUID()
+    @IsOptional()
+    @ApiPropertyOptional()
+    userId?: string
+
+    @IsUUID()
+    @IsOptional()
+    @ApiPropertyOptional()
+    methodPayId?: string
+
+    @IsBoolean()
+    @IsOptional()
+    @ApiPropertyOptional()
+    finished?: boolean
+}
+ 
