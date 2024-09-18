c97c578b8f21df4b5a10ff414ae6377b40d0503f
Eduardo Ruiz <ruizzmarin20@gmail.com>
2024-09-17 23:56:00 -0300

    cambios en coupons y vet

--- src/modules/coupons/dto/create-coupon.dto.ts
+++ src/modules/coupons/dto/create-coupon.dto.ts
@@ -1,25 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsNumber, IsDateString, IsBoolean, Min, Max } from 'class-validator';
-
+import { IsNotEmpty, IsString, IsNumber, IsBoolean, IsUUID, Min, Max } from 'class-validator';
 export class CreateCouponDto {
-  @ApiProperty({example: 'SUMMER2024'})
+  @ApiProperty({ example: 'SUMMER2024' })
   @IsNotEmpty()
   @IsString()
   code: string;
 
-  @ApiProperty({example: 15.5})
+  @ApiProperty({ example: 5000 })
+  @IsNotEmpty()
+  @IsNumber()
+  valorPesos: number;
+
+  @ApiProperty({ example: 15.5 })
   @IsNotEmpty()
   @IsNumber()
   @Min(0)
   @Max(100)
-  discount: number;
+  valorPorc: number;
 
-  @ApiProperty({example: new Date('2024-12-31')})
-  @IsNotEmpty()
-  @IsDateString()
-  expirationDate: Date;
-
-  @ApiProperty({example: true})
+  @ApiProperty({ example: true })
   @IsBoolean()
-  isActive: boolean;
+  used: boolean;
 }
